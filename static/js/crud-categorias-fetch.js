dec46b35f890ef62c1cef8ae1fb5be4239ca3604
Pablo Costa <46192942+pcosta0@users.noreply.github.com>
2024-06-25 21:07:53 -0300

    crear form categoria, listado y estilos

---
+++ static/js/crud-categorias-fetch.js
@@ -0,0 +1,154 @@
+const BASEURL = "http://127.0.0.1:5000";
+
+/**
+ * Función para realizar una petición fetch con JSON.
+ * @param {string} url - La URL a la que se realizará la petición.
+ * @param {string} method - El método HTTP a usar (GET, POST, PUT, DELETE, etc.).
+ * @param {Object} [data=null] - Los datos a enviar en el cuerpo de la petición.
+ * @returns {Promise<Object>} - Una promesa que resuelve con la respuesta en formato JSON.
+ */
+async function fetchData(url, method, data = null) {
+  const options = {
+      method: method,
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: data ? JSON.stringify(data) : null,  // Si hay datos, los convierte a JSON y los incluye en el cuerpo
+  };
+  try {
+    const response = await fetch(url, options);  // Realiza la petición fetch
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    return await response.json();  // Devuelve la respuesta en formato JSON
+  } catch (error) {
+    console.error('Fetch error:', error);
+    alert('An error occurred while fetching data. Please try again.');
+  }
+}  
+
+/**
+ * Funcion que permite crear un elemento <tr> para la tabla de peliculas
+ * por medio del uso de template string de JS.
+ */
+async function showCategorias() {
+  let categorias = await fetchData(BASEURL + "/api/categorias/", "GET");
+  const tableCategorias = document.querySelector(
+    "#list-table-categorias tbody"
+  );
+  tableCategorias.innerHTML = "";
+  categorias.forEach((categoria, index) => {
+    let tr = `<tr>
+    <td>${categoria.nombre}</td>
+    <td>${categoria.descripcion}</td>
+    <td>${categoria.activo}</td>
+    <td>
+    <button class="" onclick='updateCategoria(${categoria.id_categoria})'><i class="fa fa-pencil" ></button></i>
+    <button class="" onclick='deleteCategoria(${categoria.id_categoria})'><i class="fa fa-trash" ></button></i>
+    </td>
+    </tr>`;
+    tableCategorias.insertAdjacentHTML("beforeend", tr);
+  });
+}
+
+/**
+ * Función para comunicarse con el servidor para poder Crear o Actualizar
+ * un registro de categoria
+ * @returns
+ */
+async function saveCategoria() {
+  const idCategoria = document.querySelector("#id_categoria").value;
+  const nombre = document.querySelector("#nombre").value;
+  const descripcion = document.querySelector("#descripcion").value;
+  const activo = document.querySelector("#activo").checked ? 1 : 0;
+  // VALIDACION DE FORMULARIO
+  if (!nombre || !descripcion) {
+    Swal.fire({
+      title: "Error!",
+      text: "Por favor completa todos los campos.",
+      icon: "error",
+      confirmButtonText: "Cerrar",
+    });
+    return;
+  }
+  // Crea un objeto con los datos de la película
+  const CategoriaData = {
+    nombre: nombre,
+    descripcion: descripcion,
+    activo: activo,
+  };
+  let result = null;
+  // Si hay un idCategoria, realiza una petición PUT para actualizar la Categoria existente
+  if (idCategoria !== "") {
+    result = await fetchData(
+      `${BASEURL}/api/categorias/${idCategoria}`,
+      "PUT",
+      CategoriaData
+    );
+  } else {
+    // Si no hay idCategoria, realiza una petición POST para crear una nueva Categoria
+    result = await fetchData(
+      `${BASEURL}/api/categorias/`,
+      "POST",
+      CategoriaData
+    );
+  }
+  const formCategoria = document.querySelector("#form-categoria");
+  formCategoria.reset();
+  Swal.fire({
+    title: "Exito!",
+    text: result.message,
+    icon: "success",
+    confirmButtonText: "Cerrar",
+  });
+  showCategorias();
+}
+
+/**
+ * Function que permite cargar el formulario con los datos de la categoria
+ * para su edición
+ * @param {number} id Id de la categoria que se quiere editar
+ */
+async function updateCategoria(id) {
+  // Buscamos en el servidor la categoria de acuerdo al id
+  let response = await fetchData(`${BASEURL}/api/categorias/${id}`, "GET");
+  const idCategoria = document.querySelector("#id_categoria");
+  const nombre = document.querySelector("#nombre");
+  const descripcion = document.querySelector("#descripcion");
+  const activo = document.querySelector("#activo");
+  idCategoria.value = response.id_categoria;
+  nombre.value = response.nombre;
+  descripcion.value = response.descripcion;
+  activo.checked = response.activo != 0;
+}
+
+/**
+ * Function que permite eliminar una categoria del array del localstorage
+ * de acuedo al indice del mismo
+ * @param {number} id posición del array que se va a eliminar
+ */
+function deleteCategoria(id) {
+  Swal.fire({
+    title: "Esta seguro de eliminar la categoria?",
+    showCancelButton: true,
+    confirmButtonText: "Eliminar",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      let response = await fetchData(
+        `${BASEURL}/api/categorias/${id}`,
+        "DELETE"
+      );
+      showCategorias();
+      Swal.fire(response.message, "", "success");
+    }
+  });
+}
+
+// Escuchar el evento 'DOMContentLoaded' que se dispara cuando el
+// contenido del DOM ha sido completamente cargado y parseado.
+document.addEventListener("DOMContentLoaded", function () {
+  const btnSaveCategoria = document.querySelector("#btn-save-categoria");
+  // ASOCIAR UNA FUNCION AL EVENTO CLICK DEL BOTON
+  btnSaveCategoria.addEventListener("click", saveCategoria);
+  showCategorias();
+});
